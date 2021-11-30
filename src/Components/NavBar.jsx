eb5437833213540192005d9cdb67ce1a719beb44
Chintan Karki <karkichintan2000@gmail.com>
2021-11-30 23:52:14 +0545

    navbar setup 2

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,40 +1,5 @@
-// import React from "react";
-import logo from "../assets/logoPrimary.svg";
-
-// export default function NavBar() {
-// 	return (
-// 		// <section classNameNameName="header relative w-11/12 bg-gray-100 mt-16 block">
-// 		// 	<img src={logo} alt="logo" classNameNameName=" ml-24  inline-block fixed " />
-// 		// 	<button
-// 		// 		classNameNameName=" absolute bg-indigo-500  text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-10 mb-1 ease-linear transition-all duration-150"
-// 		// 		type="button"
-// 		// 	>
-// 		// 		{" "}
-// 		// 		LOGIN
-// 		// 	</button>
-// 		// </section>
-
-// 		<nav classNameName="flex items-center justify-between flex-wrap  p-6 w-5/6 m-auto sticky">
-// 			<img
-// 				src={logo}
-// 				alt="logo"
-// 				classNameNameName=" ml-24  inline-block fixed "
-// 			/>
-// 			{/* create a button component on the right side */}
-
-// 			<div classNameName=" float-right object-right">
-// 				<button classNameName=" flex items-center px-3 py-2 border rounded bg-indigo-500  text-white active:bg-pink-600 font-bold uppercase text-xs ">
-// 					Login
-// 				</button>
-// 				<button classNameName=" px-3 py-2 border rounded bg-indigo-500  text-white active:bg-pink-600 font-bold uppercase text-xs ">
-// 					Sign Up
-// 				</button>
-// 			</div>
-// 		</nav>
-// 	);
-// }
-
 import React from "react";
+import logo from "../assets/logoPrimary.svg";
 
 export default function Navbar({ fixed }) {
 	const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -82,14 +47,16 @@ export default function Navbar({ fixed }) {
 								<a
 									className="px-3 py-2 flex items-center  text-xs uppercase font-bold leading-snug text-gray hover:opacity-75"
 									href="#sign up"
-								>SIGN UP
+								>
+									SIGN UP
 								</a>
 							</li>
 							<li className="nav-item">
 								<a
 									className="px-3 py-2 flex items-center border rounded bg-indigo-500  text-white active:bg-pink-600 text-xs uppercase font-bold leading-snug text-gray hover:opacity-75"
 									href="#login"
-								>LOG IN
+								>
+									LOG IN
 								</a>
 							</li>
 						</ul>
