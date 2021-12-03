aa21c7034aa118f43d6d30bcf464d24f61e3cbab
Chintan Karki <karkichintan2000@gmail.com>
2021-12-03 23:52:48 +0545

    form validation addded

--- src/Components/LogIn/index.jsx
+++ src/Components/LogIn/index.jsx
@@ -3,9 +3,20 @@ import sideImage from "../../assets/Saly-26.svg";
 import logo from "../../assets/logoPrimary.svg";
 import smile from "../../assets/smile.png";
 import smile2 from "../../assets/smile2.png";
-import PasswordField from "../SignUp/PasswordField/PasswordField";
+import { useForm } from "react-hook-form";
+import ErrorMessage from "../Global Components/ErrorMessage";
 
 export default function LogIn() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<div className=" flex items-center min-h-screen bg-indigo-50">
 			<div className="relative flex-1 h-full max-w-4xl mx-auto rounded-xl shadow-xl transition bg-white">
@@ -21,7 +32,7 @@ export default function LogIn() {
 				/>
 				<div className="flex flex-col md:flex-row ">
 					<div className="h-32 md:h-auto bg-green-200 md:w-2/5 rounded-xl flex justify-center items-center">
-						<img className="md:h-60 md:" src={sideImage} alt="img" />
+						<img className="md:h-52 " src={sideImage} alt="img" />
 					</div>
 					<div className="flex items-center justify-center p-6 sm:p-12 md:w-3/5 bg-white rounded-xl">
 						<div className="w-full">
@@ -31,20 +42,55 @@ export default function LogIn() {
 							<h1 className="mb-4 text-2xl font-bold text-left text-indigo-900 ">
 								Log In
 							</h1>
+							<form onSubmit={handleSubmit(onSubmit)}>
+								<div className="mt-4">
+									<label className="block text-sm mb-1" htmlFor="email">
+										Email
+									</label>
+									<input
+										{...register("email", {
+											required: "Email is required",
+											maxLength: {
+												value: 30,
+												message: "You cannot exceed the length",
+											},
+										})}
+										type="email"
+										className="w-full px-4 py-2 text-sm border rounded-md bg-white focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+										placeholder="Email Address"
+									/>
+									{errors.email && (
+										<ErrorMessage message={errors.email.message} />
+									)}
+								</div>
 
-							<div className="mt-4">
-								<label className="block text-sm mb-1">Email</label>
-								<input
-									type="email"
-									className="w-full px-4 py-2 text-sm border rounded-md bg-white focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
-									placeholder="Email Address"
-								/>
-							</div>
+								{/* Password   */}
+								<div className="mt-4">
+									<label className="block text-sm mb-1">Password</label>
+									<input
+										{...register("password", {
+											required: "Password is required",
+											minLength: {
+												value: 8,
+												message: "Minimum 8 Characters required.",
+											},
+										})}
+										type="password"
+										className="w-full px-4 py-2 text-sm border rounded-md bg-white focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+										placeholder="Password"
+									/>
+									{errors.password && (
+										<ErrorMessage message={errors.password.message} />
+									)}
+								</div>
 
-							<PasswordField placeholder="Password" />
-							<button className="block w-full px-4 py-2 mt-8 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-indigo-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
-								Log In
-							</button>
+								<button
+									type="submit"
+									className="block w-full px-4 py-2 mt-8 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-indigo-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+								>
+									Log In
+								</button>
+							</form>
 
 							<div className="mt-4 text-left">
 								<p className="text-sm">
--- src/Components/SignUp/Index.jsx
+++ src/Components/SignUp/Index.jsx
@@ -3,10 +3,26 @@ import sideImage from "../../assets/Saly-25.svg";
 import logo from "../../assets/logoPrimary.svg";
 import smile from "../../assets/smile.png";
 import smile2 from "../../assets/smile2.png";
+import { useForm } from "react-hook-form";
 
-import PasswordField from "./PasswordField/PasswordField";
+import ErrorMessage from "../Global Components/ErrorMessage";
+import { useRef } from "react";
 
 export default function SignUp() {
+	const {
+		register,
+		watch,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const password = useRef({});
+	password.current = watch("password", "");
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<div className=" flex items-center min-h-screen bg-indigo-50">
 			<div className="relative flex-1 h-full max-w-4xl mx-auto rounded-xl shadow-xl transition bg-white">
@@ -21,8 +37,8 @@ export default function SignUp() {
 					alt=""
 				/>
 				<div className="flex flex-col md:flex-row ">
-					<div className="h-32 md:h-auto bg-green-200 md:w-2/5 rounded-xl flex justify-center">
-						<img  src={sideImage} alt="img" />
+					<div className="h-32 md:h-auto bg-green-200 md:w-2/5 rounded-xl flex justify-center items-center">
+						<img className="md:w-5/6" src={sideImage} alt="img" />
 					</div>
 					<div className="flex items-center justify-center p-6 sm:p-12 md:w-3/5 bg-white rounded-xl">
 						<div className="w-full">
@@ -32,27 +48,90 @@ export default function SignUp() {
 							<h1 className="mb-4 text-2xl font-bold text-left text-indigo-900 ">
 								Sign up
 							</h1>
-							<div>
-								<label className="block text-sm">Name</label>
-								<input
-									type="text"
-									className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
-									placeholder="Name"
-								/>
-							</div>
-							<div className="mt-4">
-								<label className="block text-sm">Email</label>
-								<input
-									type="email"
-									className="w-full px-4 py-2 text-sm border rounded-md bg-white focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
-									placeholder="Email Address"
-								/>
-							</div>
-							<PasswordField placeholder="Password" />
-							<PasswordField placeholder="Confirm Password" />
-							<button className="block w-full px-4 py-2 mt-8 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-indigo-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
-								Sign up
-							</button>
+							<form onSubmit={handleSubmit(onSubmit)}>
+								<div>
+									<label className="block text-sm" htmlFor="name">
+										Name
+									</label>
+									<input
+										type="text"
+										className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+										placeholder="Name"
+										{...register("name", {
+											required: "Name is required",
+											minLength: {
+												value: 3,
+												message: "Minimum 3 Characters required.",
+											},
+										})}
+									/>
+									{errors.name && (
+										<ErrorMessage message={errors.name.message} />
+									)}
+								</div>
+								<div className="mt-4">
+									<label className="block text-sm" htmlFor="email">
+										Email
+									</label>
+									<input
+										type="email"
+										{...register("email", {
+											required: "Email is required",
+											maxLength: {
+												value: 30,
+												message: "You cannot exceed the length",
+											},
+										})}
+										className="w-full px-4 py-2 text-sm border rounded-md bg-white focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+										placeholder="Email Address"
+									/>
+									{errors.email && (
+										<ErrorMessage message={errors.email.message} />
+									)}
+								</div>
+								<div className="mt-4">
+									<label className="block text-sm mb-1">Password</label>
+									<input
+										{...register("password", {
+											required: "Password is required",
+											minLength: {
+												value: 8,
+												message: "Minimum 8 Characters required.",
+											},
+										})}
+										type="password"
+										className=" relative w-full px-4 py-2 text-sm border rounded-md bg-white focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+										placeholder="Password"
+									/>
+									{errors.password && (
+										<ErrorMessage message={errors.password.message} />
+									)}
+								</div>
+								<div className="mt-4">
+									<label className="block text-sm mb-1">Confirm Password</label>
+									<input
+										{...register("confirmpassword", {
+											required: "Password is required",
+											minLength: {
+												value: 8,
+												message: "Minimum 8 Characters required.",
+											},
+											validate: (value) =>
+												value === password.current ||
+												"The passwords do not match",
+										})}
+										type="password"
+										className="w-full px-4 py-2 text-sm border rounded-md bg-white focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+										placeholder="Confirm Password"
+									/>
+									{errors.confirmpassword && (
+										<ErrorMessage message={errors.confirmpassword.message} />
+									)}
+								</div>
+								<button className="block w-full px-4 py-2 mt-8 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-indigo-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
+									Sign up
+								</button>
+							</form>
 
 							<div className="mt-4 text-left">
 								<p className="text-sm">
