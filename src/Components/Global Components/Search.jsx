01064d05ad2ea2b694fce315e5238e03895fc020
Chintan Karki <karkichintan2000@gmail.com>
2021-12-09 23:52:39 +0545

    tailwind cleanup continued

--- src/Components/Global Components/Search.jsx
+++ src/Components/Global Components/Search.jsx
@@ -8,6 +8,10 @@ export default function Search() {
 	const twClasses = {
 		mainDiv:
 			"relative flex flex-col  justify-center items-center  md:flex-row w-screen",
+		searchInputDiv:
+			"rounded-lg p-4 m-2  h-12 border-t border-b border-l text-gray-800 bg-white md:w-6/12 w-11/12 min-w-12 shadow-2xl max-w-md",
+		searchButton:
+			"py-2 m-2 rounded-lg h-12 md:w-36 w-11/12 bg-indigo-500  font-bold px-9 uppercase text-gray-50 border-t border-b border-r shadow-2xl max-w-md",
 	};
 
 	const onSubmit = (data) => {
@@ -27,13 +31,13 @@ export default function Search() {
 			<div style={{ height: "90vh" }} className={twClasses.mainDiv}>
 				<input
 					{...register("searchQuery")}
-					className=" rounded-lg p-4 m-2  h-12 border-t border-b border-l text-gray-800 bg-white md:w-6/12 w-11/12 min-w-12 shadow-2xl max-w-md  "
+					className={twClasses.searchInputDiv}
 					placeholder="Search 🙂"
 				/>
 				<button
 					type="submit"
 					onClick={handleSubmit(onSubmit)}
-					className="py-2 m-2 rounded-lg h-12 md:w-36 w-11/12 bg-indigo-500  font-bold px-9 uppercase text-gray-50 border-t border-b border-r shadow-2xl max-w-md"
+					className={twClasses.searchButton}
 				>
 					SEARCH
 				</button>
