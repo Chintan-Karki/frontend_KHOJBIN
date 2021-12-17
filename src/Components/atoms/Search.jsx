dd64b64cfee391955b781d17fbf07df76e4c3167
Chintan Karki <karkichintan2000@gmail.com>
2021-12-17 23:03:21 +0545

    index setups

--- src/Components/atoms/Search.jsx
+++ src/Components/atoms/Search.jsx
@@ -9,9 +9,9 @@ export default function Search() {
 		mainDiv:
 			"h-[90vh] relative flex flex-col justify-center items-center  md:flex-row w-screen",
 		searchInputDiv:
-			"rounded-lg p-4 m-2  h-12 border-t border-b border-l text-gray-800 bg-white md:w-6/12 w-11/12 min-w-12 shadow-lg shadow-indigo-500/30 max-w-md",
+			"rounded-lg p-4 m-2  h-12 border-t border-b border-l text-gray-800 bg-white md:w-6/12 w-11/12 min-w-12 shadow-lg shadow-indigo-500/50 max-w-md",
 		searchButton:
-			"py-2 m-2 border-none active:scale-90 transition ease-in-out duration-200 rounded-lg h-12 md:w-36 w-11/12 bg-indigo-500  font-bold px-9 uppercase text-gray-50 border-t border-b border-r shadow-lg shadow-indigo-500/30 max-w-md",
+			"py-2 m-2 border-none active:scale-90 transition ease-in-out duration-200 rounded-lg h-12 md:w-36 w-11/12 bg-indigo-500  font-bold px-9 uppercase text-gray-50 border-t border-b border-r shadow-lg hover:shadow-indigo-500/50 hover:bg-indigo-600 max-w-md",
 	};
 
 	const onSubmit = (data) => {
