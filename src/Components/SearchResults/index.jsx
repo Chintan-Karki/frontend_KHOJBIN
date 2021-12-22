0efe4310af6824ed5978d6f69d714abd3bf95152
Chintan Karki <karkichintan2000@gmail.com>
2021-12-22 23:49:10 +0545

    test products

--- src/Components/SearchResults/index.jsx
+++ src/Components/SearchResults/index.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "../atoms/NavBar";
+import Product from "../Products/Product";
 
 export default function SearchResults() {
 	const location = useLocation();
-	let searchQuery = location.state.searchQuery.trim();
-	let searchTime = location.state.searchTime;
-
+	let searchQuery = location.state.searchQuery
+		? location.state.searchQuery
+		: "";
+	let searchTime = location.state.searchTime ? location.state.searchTime : "";
+	let productsArray = [1, 2, 3, 4, 5];
 	return (
 		<>
 			<Navbar />
-			<div className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3">
-				<div className="container px-4 mx-auto items-center justify-between">
-					<h1 className="text-7xl text-slate-900">
-						You searched for "{searchQuery}"
-					</h1>
-					<br />
-					<span>Time of search : {searchTime.toUTCString()}</span>
+			{(searchQuery !== "") | (searchTime !== "") ? (
+				<div className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3">
+					<div className="container px-4 mx-auto items-center justify-between">
+						<h1 className="text-7xl text-slate-900">
+							You searched for "{searchQuery}"
+						</h1>
+						<br />
+						<span>Time of search : {searchTime.toUTCString()}</span>
+						{productsArray.map((product) => (
+							<Product product={product} />
+						))}
+					</div>
 				</div>
-			</div>
-			;
-
+			) : (
+				<h1>No search query found</h1>
+			)}
 		</>
 	);
 }
--- src/assets/commonClasses.tailwind.js
+++ src/assets/commonClasses.tailwind.js
@@ -1,9 +1,8 @@
 let tailwindCommonClasses = {
 	smallButtonNofocus:
-		"px-3 py-2 flex items-center text-xs uppercase hover:bg-indigo-700 hover:shadow-indigo-500/60 hover:text-white rounded-lg border-none active:scale-90 hover:shadow-lg active:text-slate-50 transition ease-in-out duration-50 font-bold leading-snug text-gray",
+		"px-3 py-2 border-2 flex items-center text-xs uppercase hover:bg-indigo-700 hover:shadow-indigo-500/60 hover:text-white rounded-lg border-none active:scale-90 hover:shadow-lg active:text-slate-50 transition ease-in-out duration-50 font-bold leading-snug text-gray",
 	smallButtonFocus:
 		"px-3 py-2 flex items-center text-xs uppercase hover:bg-indigo-700 hover:shadow-indigo-500/60 hover:text-white rounded-lg border-none active:scale-90 hover:shadow-lg active:text-slate-50 transition ease-in-out duration-50 font-bold leading-snug text-gray-50 bg-indigo-500 shadow-indigo-500/60 ",
 };
 
 export default tailwindCommonClasses;
-
